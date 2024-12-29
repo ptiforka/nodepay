b53bf939efe56d71a14bc0cd02b8a5bf7f47f7ac
ptiforka <35385354+ptiforka@users.noreply.github.com>
2024-12-29 12:54:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,22 @@
+# https://github.com/im-hanzou/nodepay-automate
 import asyncio
-import cloudscraper
-import json
+import aiohttp
 import time
+import uuid
+import cloudscraper
 from loguru import logger
-import requests
-from concurrent.futures import ThreadPoolExecutor
+from fake_useragent import UserAgent
 
-PING_INTERVAL = 15
-RETRIES = 10
+def show_warning():
+    print("Continuing...")
+
+# Constants
+PING_INTERVAL = 60
+RETRIES = 60
 
 DOMAIN_API = {
     "SESSION": "http://api.nodepay.ai/api/auth/session",
-    
-    "PING": [
-        "https://nw.nodepay.org/api/network/ping"
-    
-    ]
+    "PING": "http://nw.nodepay.ai/api/network/ping"
 }
 
 CONNECTION_STATES = {
@@ -24,39 +25,163 @@ CONNECTION_STATES = {
     "NONE_CONNECTION": 3
 }
 
-BASE_PROXY = "CHANGE YOUR BASE PROXY"
+status_connect = CONNECTION_STATES["NONE_CONNECTION"]
+browser_id = None
+account_info = {}
+last_ping_time = {}  
 
+def uuidv4():
+    return str(uuid.uuid4())
+    
+def valid_resp(resp):
+    if not resp or "code" not in resp or resp["code"] < 0:
+        raise ValueError("Invalid response")
+    return resp
+    
+async def render_profile_info(proxy, token):
+    global browser_id, account_info
 
-class AccountInfo:
-    def __init__(self, token):
-        self.token = token
-        self.proxies = [BASE_PROXY] * 3  # Each account uses the same proxy up to 3 times
-        self.status_connect = CONNECTION_STATES["NONE_CONNECTION"]
-        self.account_data = {}
-        self.retries = 0
-        self.last_ping_status = 'Waiting...'
-        self.browser_id = {
-            'ping_count': 0,
-            'successful_pings': 0,
-            'score': 0,
-            'start_time': time.time(),
-            'last_ping_time': None
+    try:
+        np_session_info = load_session_info(proxy)
+
+        if not np_session_info:
+            # Generate new browser_id
+            browser_id = uuidv4()
+            response = await call_api(DOMAIN_API["SESSION"], {}, proxy, token)
+            valid_resp(response)
+            account_info = response["data"]
+            if account_info.get("uid"):
+                save_session_info(proxy, account_info)
+                await start_ping(proxy, token)
+            else:
+                handle_logout(proxy)
+        else:
+            account_info = np_session_info
+            await start_ping(proxy, token)
+    except Exception as e:
+        logger.error(f"Error in render_profile_info for proxy {proxy}: {e}")
+        error_message = str(e)
+        if any(phrase in error_message for phrase in [
+            "sent 1011 (internal error) keepalive ping timeout; no close frame received",
+            "500 Internal Server Error"
+        ]):
+            logger.info(f"Removing error proxy from the list: {proxy}")
+            remove_proxy_from_list(proxy)
+            return None
+        else:
+            logger.error(f"Connection error: {e}")
+            return proxy
+
+async def call_api(url, data, proxy, token):
+    user_agent = UserAgent(os=['windows', 'macos', 'linux'], browsers='chrome')
+    random_user_agent = user_agent.random
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "User-Agent": random_user_agent,
+        "Content-Type": "application/json",
+        "Origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm",
+        "Accept": "application/json",
+        "Accept-Language": "en-US,en;q=0.5",
+    }
+
+    try:
+        scraper = cloudscraper.create_scraper()
+
+        response = scraper.post(url, json=data, headers=headers, proxies={
+                                "http": proxy, "https": proxy}, timeout=30)
+
+        response.raise_for_status()
+        return valid_resp(response.json())
+    except Exception as e:
+        logger.error(f"Error during API call: {e}")
+        raise ValueError(f"Failed API call to {url}")
+
+async def start_ping(proxy, token):
+    try:
+        while True:
+            await ping(proxy, token)
+            await asyncio.sleep(PING_INTERVAL)
+    except asyncio.CancelledError:
+        logger.info(f"Ping task for proxy {proxy} was cancelled")
+    except Exception as e:
+        logger.error(f"Error in start_ping for proxy {proxy}: {e}")
+        
+async def ping(proxy, token):
+    global last_ping_time, RETRIES, status_connect
+
+    current_time = time.time()
+
+    if proxy in last_ping_time and (current_time - last_ping_time[proxy]) < PING_INTERVAL:
+        logger.info(f"Skipping ping for proxy { proxy}, not enough time elapsed")
+        return
+
+    last_ping_time[proxy] = current_time
+
+    try:
+        data = {
+            "id": account_info.get("uid"),
+            "browser_id": browser_id,  
+            "timestamp": int(time.time()),
+            "version": "2.2.7"
         }
 
-    def reset(self):
-        self.status_connect = CONNECTION_STATES["NONE_CONNECTION"]
-        self.account_data = {}
-        self.retries = 3
+        response = await call_api(DOMAIN_API["PING"], data, proxy, token)
+        if response["code"] == 0:
+            logger.info(f"Ping successful via proxy {proxy}: {response}")
+            RETRIES = 0
+            status_connect = CONNECTION_STATES["CONNECTED"]
+        else:
+            handle_ping_fail(proxy, response)
+    except Exception as e:
+        logger.error(f"Ping failed via proxy {proxy}: {e}")
+        handle_ping_fail(proxy, None)
 
+def handle_ping_fail(proxy, response):
+    global RETRIES, status_connect
 
-scraper = cloudscraper.create_scraper(
-    browser={
-        'browser': 'chrome',
-        'platform': 'windows',
-        'desktop': True
+    RETRIES += 1
+    if response and response.get("code") == 403:
+        handle_logout(proxy)
+    elif RETRIES < 2:
+        status_connect = CONNECTION_STATES["DISCONNECTED"]
+    else:
+        status_connect = CONNECTION_STATES["DISCONNECTED"]
+
+def handle_logout(proxy):
+    global status_connect, account_info
+
+    status_connect = CONNECTION_STATES["NONE_CONNECTION"]
+    account_info = {}
+    save_status(proxy, None)
+    logger.info(f"Logged out and cleared session info for proxy {proxy}")
+
+def load_proxies(proxy_file):
+    try:
+        with open(proxy_file, 'r') as file:
+            proxies = file.read().splitlines()
+        return proxies
+    except Exception as e:
+        logger.error(f"Failed to load proxies: {e}")
+        raise SystemExit("Exiting due to failure in loading proxies")
+
+def save_status(proxy, status):
+    pass  
+
+def save_session_info(proxy, data):
+    data_to_save = {
+        "uid": data.get("uid"),
+        "browser_id": browser_id  
     }
-)
+    pass
 
+def load_session_info(proxy):
+    return {}  
+
+def is_valid_proxy(proxy):
+    return True  
+
+def remove_proxy_from_list(proxy):
+    pass  
 
 async def load_tokens():
     try:
@@ -70,113 +195,46 @@ async def load_tokens():
     except Exception as e:
         logger.error(f"Failed to load tokens: {e}")
         raise SystemExit("Exiting due to failure in loading tokens")
-
-async def call_api(url, data, account_info, proxy):
-    headers = {
-        "Authorization": f"Bearer {account_info.token}",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Referer": "https://app.nodepay.ai/",
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-        "Origin": "chrome-extension://lgmpfmgeabnnlemejacfljbmonaomfmm",
-        "Sec-Ch-Ua": '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
-        "Sec-Ch-Ua-Mobile": "?0",
-        "Sec-Ch-Ua-Platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "cors-site"
-    }
-
-    proxy_config = {
-        "http": proxy,
-        "https": proxy
-    }
-
-    try:
-        response = scraper.post(url, json=data, headers=headers, proxies=proxy_config, timeout=60)
-        response.raise_for_status()
-    except Exception as e:
-        logger.error(f"Error during API call for token {account_info.token} with proxy {proxy}: {e}")
-        raise ValueError(f"Failed API call to {url}")
-
-    return response.json()
-
-
-async def render_profile_info(account_info):
-    try:
-        for proxy in account_info.proxies:
-            try:
-                response = await call_api(DOMAIN_API["SESSION"], {}, account_info, proxy)
-                if response.get("code") == 0:
-                    account_info.account_data = response["data"]
-                    if account_info.account_data.get("uid"):
-                        await start_ping(account_info)
-                        return
-                else:
-                    logger.warning(f"Session failed for token {account_info.token} using proxy {proxy}")
-            except Exception as e:
-                logger.error(f"Failed to render profile info for token {account_info.token} using proxy {proxy}: {e}")
-
-        logger.error(f"All proxies failed for token {account_info.token}")
-    except Exception as e:
-        logger.error(f"Error in render_profile_info for token {account_info.token}: {e}")
-
-
-async def start_ping(account_info):
-    try:
-        logger.info(f"Starting ping for token {account_info.token}")
-        while True:
-            for proxy in account_info.proxies:
-                try:
-                    await asyncio.sleep(PING_INTERVAL)
-                    await ping(account_info, proxy)
-                except Exception as e:
-                    logger.error(f"Ping failed for token {account_info.token} using proxy {proxy}: {e}")
-    except asyncio.CancelledError:
-        logger.info(f"Ping task for token {account_info.token} was cancelled")
-    except Exception as e:
-        logger.error(f"Error in start_ping for token {account_info.token}: {e}")
-
-
-async def ping(account_info, proxy):
-    for url in DOMAIN_API["PING"]:
-        try:
-            data = {
-                "id": account_info.account_data.get("uid"),
-                "browser_id": account_info.browser_id,
-                "timestamp": int(time.time())
-            }
-            response = await call_api(url, data, account_info, proxy)
-            if response["code"] == 0:
-                logger.info(f"Ping successful for token {account_info.token} using proxy {proxy}")
-                return
-        except Exception as e:
-            logger.error(f"Ping failed for token {account_info.token} using URL {url} and proxy {proxy}: {e}")
-
-
-def process_account(token):
-    """
-    Process a single account: Initialize proxies and start asyncio event loop for this account.
-    """
-    account_info = AccountInfo(token)
-    asyncio.run(render_profile_info(account_info))
-
-
+        
 async def main():
-    tokens = await load_tokens()
+    all_proxies = load_proxies('local_proxies.txt')
+    # Take token input directly from the user
+    token = input("Nodepay token: ").strip()
+    if token == "#":
+        print("Exiting...")
+        return
+    
+    while True:
+        active_proxies = [
+            proxy for proxy in all_proxies if is_valid_proxy(proxy)][:100]
+        tasks = {asyncio.create_task(render_profile_info(
+            proxy, token)): proxy for proxy in active_proxies}
 
-    with ThreadPoolExecutor(max_workers=1000) as executor:
-        futures = []
-        for token in tokens:
-            futures.append(executor.submit(process_account, token))
+        done, pending = await asyncio.wait(tasks.keys(), return_when=asyncio.FIRST_COMPLETED)
+        for task in done:
+            failed_proxy = tasks[task]
+            if task.result() is None:
+                logger.info(f"Removing and replacing failed proxy: {failed_proxy}")
+                active_proxies.remove(failed_proxy)
+                if all_proxies:
+                    new_proxy = all_proxies.pop(0)
+                    if is_valid_proxy(new_proxy):
+                        active_proxies.append(new_proxy)
+                        new_task = asyncio.create_task(
+                            render_profile_info(new_proxy, token))
+                        tasks[new_task] = new_proxy
+            tasks.pop(task)
 
-        # Wait for all tasks to complete
-        for future in futures:
-            future.result()  # Block until each future is done
-
+        for proxy in set(active_proxies) - set(tasks.values()):
+            new_task = asyncio.create_task(
+                render_profile_info(proxy, token))
+            tasks[new_task] = proxy
+        await asyncio.sleep(3)
+    await asyncio.sleep(10)  
 
 if __name__ == '__main__':
+    show_warning()
+    print("\nAlright, we here! Insert your nodepay token that you got from the tutorial.")
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
